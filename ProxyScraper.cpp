1d257553ff0226848ce514ce50ec2500755df9d5
CHA1NSK1 <96607632+Chainski@users.noreply.github.com>
2025-07-23 08:54:06 -0500

    Add files via upload

--- ProxyScraper.cpp
+++ ProxyScraper.cpp
@@ -8,6 +8,8 @@
 #include <thread>
 #include <chrono>
 #include <algorithm>
+#include <regex>
+#include <unordered_set>
 #include <mutex>
 #pragma comment(lib, "winhttp.lib")
 #pragma comment(lib, "ws2_32.lib")
@@ -36,70 +38,66 @@ void enableANSIColors() {
 #endif
 }
 
+void CleanProxyFile(const string& filename) {
+    ifstream in(filename);
+    if (!in) return;
+    unordered_set<string> unique;
+    vector<string> valid_lines;
+    regex proxy_regex(R"(^\d{1,3}(?:\.\d{1,3}){3}:\d{2,5}$)");
+    string line;
+    while (getline(in, line)) {
+        line.erase(remove_if(line.begin(), line.end(), ::isspace), line.end());
+        if (line.empty() ||
+            line.find("error code:") != string::npos ||
+            line.find("Invalid API request") != string::npos)
+            continue;
+        size_t badPos = line.find_first_not_of("0123456789.:");
+        if (badPos != string::npos)
+            line = line.substr(0, badPos);
+
+        if (regex_match(line, proxy_regex) && unique.insert(line).second)
+            valid_lines.push_back(line);
+    }
+    ofstream out(filename, ios::trunc);
+    for (const auto& proxy : valid_lines)
+        out << proxy << '\n';
+}
 mutex fileMutex;
-string SendRequest(const wstring& url) {
-    string result;
-    HINTERNET hSession = nullptr, hConnect = nullptr, hRequest = nullptr;
-    DWORD dwBytesRead = 0;
-    DWORD dwSize = 0;
-    vector<char> buffer(8192);
-    URL_COMPONENTS urlComp = { sizeof(URL_COMPONENTS) };
-    urlComp.dwSchemeLength = (DWORD)-1;
-    urlComp.dwHostNameLength = (DWORD)-1;
-    urlComp.dwUrlPathLength = (DWORD)-1;
-    urlComp.dwExtraInfoLength = (DWORD)-1;
-    if (!WinHttpCrackUrl(url.c_str(), 0, 0, &urlComp)) {
-        return "";
-    }
-    wstring hostName(urlComp.lpszHostName, urlComp.dwHostNameLength);
-    wstring urlPath(urlComp.lpszUrlPath, urlComp.dwUrlPathLength);
-    INTERNET_PORT port = urlComp.nScheme == INTERNET_SCHEME_HTTPS ? INTERNET_DEFAULT_HTTPS_PORT : INTERNET_DEFAULT_HTTP_PORT;
-    hSession = WinHttpOpen(L"Proxy Scraper", WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, WINHTTP_NO_PROXY_NAME, WINHTTP_NO_PROXY_BYPASS, 0);
-    if (!hSession) {
-        return "";
-    }
-    hConnect = WinHttpConnect(hSession, hostName.c_str(), port, 0);
-    if (!hConnect) {
-        WinHttpCloseHandle(hSession);
-        return "";
-    }
-    hRequest = WinHttpOpenRequest(hConnect, L"GET", urlPath.c_str(), nullptr, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES, urlComp.nScheme == INTERNET_SCHEME_HTTPS ? WINHTTP_FLAG_SECURE : 0);
-    if (!hRequest) {
-        WinHttpCloseHandle(hConnect);
-        WinHttpCloseHandle(hSession);
-        return "";
-    }
-    if (!WinHttpSendRequest(hRequest, WINHTTP_NO_ADDITIONAL_HEADERS, 0, WINHTTP_NO_REQUEST_DATA, 0, 0, 0)) {
-        WinHttpCloseHandle(hRequest);
-        WinHttpCloseHandle(hConnect);
-        WinHttpCloseHandle(hSession);
-        return "";
-    }
-    if (!WinHttpReceiveResponse(hRequest, nullptr)) {
-        WinHttpCloseHandle(hRequest);
-        WinHttpCloseHandle(hConnect);
-        WinHttpCloseHandle(hSession);
-        return "";
-    }
-    do {
-        dwSize = 0;
-        if (!WinHttpQueryDataAvailable(hRequest, &dwSize)) {
-            break;
+std::string SendRequest(const std::wstring& url) {
+    std::string result;
+    std::vector<char> buffer(8192);
+    DWORD dwBytesRead = 0, statusCode = 0, statusSize = sizeof(DWORD), dwSize = 0;
+    URL_COMPONENTS urlComp{};
+    urlComp.dwStructSize = sizeof(urlComp);
+    urlComp.dwSchemeLength = urlComp.dwHostNameLength = urlComp.dwUrlPathLength = urlComp.dwExtraInfoLength = (DWORD)-1;
+    if (!WinHttpCrackUrl(url.c_str(), 0, 0, &urlComp))
+    return result;
+    auto close = [](HINTERNET h) { if (h) WinHttpCloseHandle(h); };
+    HINTERNET hSession = WinHttpOpen(L"Mozilla/5.0 (X11; Linux x86_64; rv:140.0) Gecko/20100101 Firefox/140.0",
+    WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,WINHTTP_NO_PROXY_NAME,WINHTTP_NO_PROXY_BYPASS, 0);
+    HINTERNET hConnect = hSession ? WinHttpConnect(hSession,
+    std::wstring(urlComp.lpszHostName, urlComp.dwHostNameLength).c_str(),
+    urlComp.nScheme == INTERNET_SCHEME_HTTPS ? INTERNET_DEFAULT_HTTPS_PORT : INTERNET_DEFAULT_HTTP_PORT, 0) : nullptr;
+    HINTERNET hRequest = hConnect ? WinHttpOpenRequest(hConnect, L"GET",
+    std::wstring(urlComp.lpszUrlPath, urlComp.dwUrlPathLength).c_str(),
+    nullptr, WINHTTP_NO_REFERER, WINHTTP_DEFAULT_ACCEPT_TYPES,
+    urlComp.nScheme == INTERNET_SCHEME_HTTPS ? WINHTTP_FLAG_SECURE : 0) : nullptr;
+    bool ok = hRequest &&
+    WinHttpSendRequest(hRequest, nullptr, 0, nullptr, 0, 0, 0) &&
+    WinHttpReceiveResponse(hRequest, nullptr) &&
+    WinHttpQueryHeaders(hRequest, WINHTTP_QUERY_STATUS_CODE | WINHTTP_QUERY_FLAG_NUMBER,
+    nullptr, &statusCode, &statusSize, nullptr) &&
+    statusCode == HTTP_STATUS_OK;
+    if (ok) {
+        while (WinHttpQueryDataAvailable(hRequest, &dwSize) && dwSize > 0) {
+            if (!WinHttpReadData(hRequest, buffer.data(), std::min<DWORD>(dwSize, buffer.size()), &dwBytesRead) || dwBytesRead == 0)
+                break;
+            result.append(buffer.data(), dwBytesRead);
         }
-        if (dwSize == 0) {
-            break;
-        }
-        if (!WinHttpReadData(hRequest, buffer.data(), std::min(dwSize, static_cast<DWORD>(buffer.size())), &dwBytesRead)) {
-            break;
-        }
-        if (dwBytesRead == 0) {
-            break;
-        }
-        result.append(buffer.data(), dwBytesRead);
-    } while (dwSize > 0);
-    WinHttpCloseHandle(hRequest);
-    WinHttpCloseHandle(hConnect);
-    WinHttpCloseHandle(hSession);
+    }
+    close(hRequest);
+    close(hConnect);
+    close(hSession);
     return result;
 }
 void SaveFile(const string& filename, const string& url) {
@@ -117,7 +115,6 @@ void SaveFile(const string& filename, const string& url) {
     catch (...) {
     }
 }
-
 void ProcessUrls(const vector<string>& urls, const string& filename, size_t start, size_t end) {
     for (size_t i = start; i < end && i < urls.size(); ++i) {
         SaveFile(filename, urls[i]);
@@ -147,79 +144,65 @@ int main() {
     enableANSIColors();
     system("mode con: cols=120 lines=50");
     SetConsoleTitleA("Proxy Scraper - Made By: Chainski");
-	printBanner(); 
+    printBanner(); 
     string line;
     TWE("[+] This program will autoscrape proxies into separate files", 10);
     cout << CYAN << "[+] Scraping Proxies Please Wait . . ." << RESET << endl;
-    const size_t numThreads = 4;
-    vector<string> http_urls = {
-        "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt",
-        "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
-        "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all",
-        "https://raw.githubusercontent.com/jetkai/proxy-list/main/archive/txt/proxies-https.txt",
-        "https://raw.githubusercontent.com/jetkai/proxy-list/main/archive/txt/proxies-http.txt",
-        "https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt",
-        "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
-        "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
-        "https://www.proxy-list.download/api/v1/get?type=http",
-        "https://www.proxy-list.download/api/v1/get?type=https",
-        "https://api.openproxylist.xyz/http.txt",
-        "https://raw.githubusercontent.com/mmpx12/proxy-list/master/http.txt",
-        "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt",
-        "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
-        "https://raw.githubusercontent.com/proxy4parsing/proxy-list/main/http.txt"
+    vector<pair<vector<string>, string>> proxy_types = {
+        {{
+            "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-https.txt",
+            "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-http.txt",
+            "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all",
+            "https://raw.githubusercontent.com/jetkai/proxy-list/main/archive/txt/proxies-https.txt",
+            "https://raw.githubusercontent.com/jetkai/proxy-list/main/archive/txt/proxies-http.txt",
+            "https://raw.githubusercontent.com/roosterkid/openproxylist/main/HTTPS_RAW.txt",
+            "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/http.txt",
+            "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/http.txt",
+            "https://www.proxy-list.download/api/v1/get?type=http",
+            "https://www.proxy-list.download/api/v1/get?type=https",
+            "https://api.openproxylist.xyz/http.txt",
+            "https://raw.githubusercontent.com/mmpx12/proxy-list/master/http.txt",
+            "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/http.txt",
+            "https://api.proxyscrape.com/v2/?request=getproxies&protocol=http&timeout=10000&country=all&ssl=all&anonymity=all",
+            "https://raw.githubusercontent.com/proxy4parsing/proxy-list/main/http.txt"
+        }, "http.txt"},
+        {{
+            "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=10000&country=all",
+            "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS4_RAW.txt",
+            "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks4.txt",
+            "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt",
+            "https://www.proxy-list.download/api/v1/get?type=socks4",
+            "https://api.openproxylist.xyz/socks4.txt",
+            "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks4.txt",
+            "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks4.txt",
+            "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/socks4.txt",
+            "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks4.txt"
+        }, "socks4.txt"},
+        {{
+            "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all",
+            "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS5_RAW.txt",
+            "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks5.txt",
+            "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt",
+            "https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt",
+            "https://www.proxy-list.download/api/v1/get?type=socks5",
+            "https://api.openproxylist.xyz/socks5.txt",
+            "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks5.txt",
+            "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt",
+            "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks5.txt"
+        }, "socks5.txt"}
     };
     vector<thread> threads;
-    size_t urlsPerThread = max(size_t(1), http_urls.size() / numThreads);
-    for (size_t i = 0; i < http_urls.size(); i += urlsPerThread) {
-        size_t end = min(i + urlsPerThread, http_urls.size());
-        threads.emplace_back(ProcessUrls, cref(http_urls), "http.txt", i, end);
+    for (const auto& [urls, file] : proxy_types) {
+        cout << MAGENTA << "[!] Scraping " << file.substr(0, file.find('.')) << " proxies...\n";
+        for (size_t i = 0; i < urls.size(); i += max(size_t(1), urls.size() / 4))
+            threads.emplace_back(ProcessUrls, cref(urls), file, i, min(i + urls.size() / 4, urls.size()));
+        for (auto& t : threads) t.join();
+        threads.clear();
+        cout << CYAN << "[!] Saved " << file.substr(0, file.find('.')) << " proxies!\n";
+        this_thread::sleep_for(chrono::seconds(1));
     }
-    for (auto& t : threads) t.join();
-    threads.clear();
-    cout << RED << "[!] Successfully Scraped And Saved HTTP Proxies!" << RESET << endl;
-    this_thread::sleep_for(chrono::seconds(1));
-    vector<string> socks4_urls = {
-        "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks4&timeout=10000&country=all",
-        "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS4_RAW.txt",
-        "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks4.txt",
-        "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks4.txt",
-        "https://www.proxy-list.download/api/v1/get?type=socks4",
-        "https://api.openproxylist.xyz/socks4.txt",
-        "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks4.txt",
-        "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks4.txt",
-        "https://raw.githubusercontent.com/rdavydov/proxy-list/main/proxies/socks4.txt",
-        "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks4.txt"
-    };
-    urlsPerThread = max(size_t(1), socks4_urls.size() / numThreads);
-    for (size_t i = 0; i < socks4_urls.size(); i += urlsPerThread) {
-        size_t end = min(i + urlsPerThread, socks4_urls.size());
-        threads.emplace_back(ProcessUrls, cref(socks4_urls), "socks4.txt", i, end);
-    }
-    for (auto& t : threads) t.join();
-    threads.clear();
-    cout << LIGHT_BLUE << "[!] Successfully Scraped And Saved SOCKS4 Proxies!" << RESET << endl;
-    this_thread::sleep_for(chrono::seconds(1));
-    vector<string> socks5_urls = {
-        "https://api.proxyscrape.com/v2/?request=getproxies&protocol=socks5&timeout=10000&country=all",
-        "https://raw.githubusercontent.com/roosterkid/openproxylist/main/SOCKS5_RAW.txt",
-        "https://raw.githubusercontent.com/monosans/proxy-list/main/proxies/socks5.txt",
-        "https://raw.githubusercontent.com/TheSpeedX/PROXY-List/master/socks5.txt",
-        "https://raw.githubusercontent.com/hookzof/socks5_list/master/proxy.txt",
-        "https://www.proxy-list.download/api/v1/get?type=socks5",
-        "https://api.openproxylist.xyz/socks5.txt",
-        "https://raw.githubusercontent.com/mmpx12/proxy-list/master/socks5.txt",
-        "https://raw.githubusercontent.com/ShiftyTR/Proxy-List/master/socks5.txt",
-        "https://raw.githubusercontent.com/jetkai/proxy-list/main/online-proxies/txt/proxies-socks5.txt"
-    };
-    urlsPerThread = max(size_t(1), socks5_urls.size() / numThreads);
-    for (size_t i = 0; i < socks5_urls.size(); i += urlsPerThread) {
-        size_t end = min(i + urlsPerThread, socks5_urls.size());
-        threads.emplace_back(ProcessUrls, cref(socks5_urls), "socks5.txt", i, end);
-    }
-    for (auto& t : threads) t.join();
-    threads.clear();
-    cout << RED << "[!] Successfully Scraped And Saved SOCKS5 Proxies!" << RESET << endl;
+    for (const auto& [_, file] : proxy_types)
+    CleanProxyFile(file);
     cout << GREEN << "Press any key to continue . . .";
     cin.get();
     return 0;
